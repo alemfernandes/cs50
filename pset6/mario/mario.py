bc9dfc94b06781a1f237f538a6cdffe17615dd57
alemfernandes <55447543+alemfernandes@users.noreply.github.com>
2020-09-11 16:27:11 -0300

    Add files via upload

--- pset6/mario/mario.py
+++ pset6/mario/mario.py
@@ -8,4 +8,6 @@ while True:
         break
 # print the hashes
 for i in range(0, n, 1):
-    print(" " * (n - 1 - i) + "#" * (i + 1))
+    print(" " * (n - 1 - i) + "#" * (i + 1), end="")
+    print("" * (n + 1), end="  ")
+    print("#" * (i + 1))
